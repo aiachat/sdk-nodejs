26cd3ade483c43833224e91e89c3e938f12140c2
Vinicius Silva <79378773+viniciusgdr@users.noreply.github.com>
2024-07-05 21:16:38 -0300

    Update constants.ts

--- src/defaults/constants.ts
+++ src/defaults/constants.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'http://localhost:3002/api/v1'
+export const API_URL = 'https://api.aiachat.com.br/api/v1'
 
 export const api = axios.create({
   baseURL: API_URL
